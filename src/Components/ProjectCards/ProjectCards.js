70afddb77d73a28027f4a822c29aa1961576e1d1
Lee Graham <graham.la3@gmail.com>
2018-10-23 15:19:36 -0600

    Create ProjectCard Component and file structure

---
+++ src/Components/ProjectCards/ProjectCards.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ProjectCard extends Component {
+  constructor() {
+    super()
+    this.state = ''
+  }
+    render() {
+    return(
+      <div>
+        ProjectCard Component
+      </div>
+    )
+  }
+}
+
+export default ProjectCard
