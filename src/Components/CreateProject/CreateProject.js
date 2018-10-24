da01c6a5405b0d9b062b2effdd1dcf2a0b40bb01
Dennis <dmiller1623@gmail.com>
2018-10-24 14:48:25 -0600

    Add functions for adding materials and displaying materials

--- src/Components/CreateProject/CreateProject.js
+++ src/Components/CreateProject/CreateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './CreateProject.css'
+import deleteButton from '../../images/x-button.svg'
 
 class CreateProject extends Component {
   constructor() {
@@ -7,35 +8,51 @@ class CreateProject extends Component {
     this.state = {
       projectName: '',
       projectDescription: '',
-      materials: [],
+      newMaterial: '',
+      materials: []
     }
   }
 
   handleChange = (event) => {
     const { name, value } = event.target;
+
     this.setState({
       [name]: value
     })
   }
 
   handleSubmit = (event) => {
-    event.preventDefault()
-    this.setState({
-      projectName: '',
-      projectDescription: '',
-      materials: []
-    })
+    event.preventDefault();
+    if(event.target.className === 'submit-project-button') {
+      this.setState({
+        projectName: '',
+        projectDescription: '',
+        materials: []
+      })
+    }
+    return
   }
 
-  addMateriall = () => {
-    
+  addMaterial = (event, material) => {
+    event.preventDefault();
+    if(event.target.className === 'add-material-button') {
+      let newMaterial = {material};
+      let materials = [...this.state.materials, newMaterial];
+  
+      this.setState({
+        materials, 
+        newMaterial: ''
+      })
+    }
   }
 
   render() {
     return(
       <div className='create-project-section'>
         <form className='create-project-form' onSubmit={this.handleSubmit}>
+          <h1>Create Project</h1>
           <input
+            className='project-inputs'
             name='projectName'
             type='text'
             placeholder='Project Name'
@@ -43,14 +60,33 @@ class CreateProject extends Component {
             onChange={this.handleChange}
           />
           <textarea
+            className='project-inputs description'
             name='projectDescription'
             type='text'
             placeholder='Project description:'
             value={this.state.projectDescription}
             onChange={this.handleChange}
           />
-          <button>Submit Project</button>
+          <textarea
+            className='project-inputs materials'
+            name='newMaterial'
+            type='text'
+            placeholder='Add new material'
+            value={this.state.newMaterial}
+            onChange={this.handleChange}
+          />
+          <button className='add-material-button' onClick={(event) => this.addMaterial(event, this.state.newMaterial)}>Add Material</button>
+          <button className='submit-project-button'>Submit Project</button>
         </form>
+        <div className='listed-materials'>
+          <h1>Materials:</h1>
+            {this.state.materials.map((material, index) => {
+              return <div 
+                      className='list-item'>{`${index+1}. ${material.material}`}
+                      <img className='delete-material' src={deleteButton}/>
+                    </div>
+            })}
+        </div>
       </div>
     )
   }
