cce69722f903256898ed06c3d4079bdb6da8dc99
Dennis <dmiller1623@gmail.com>
2018-10-26 09:53:15 -0600

    Add routes to sign in with google or us and add oauth for google sign in

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -5,14 +5,32 @@ import './WelcomePage.css'
 class WelcomePage extends Component {
   constructor() {
     super()
-    this.state = ''
+    this.state = {
+      loggingIn: false
+    }
   }
+
+  handleLogin = () => {
+    let loggingIn = !this.state.loggingIn;
+    this.setState({
+      loggingIn
+    }) 
+  }
+
   render() {
+    if(this.state.loggingIn) {
+      return (
+        <div className='user-link-section'>
+          <NavLink className='user-link signin' to='/CreateUserGoogle'>Sign In with Google</NavLink>
+          <NavLink className='user-link signin' to='/CreateUser'>Create Account With Us</NavLink>
+        </div>
+      )
+    }
     return(
       <div className='user-link-section'>
         <NavLink className='user-link' to='/Landing'>View Projects</NavLink>
-        <NavLink className='user-link' to='/CreateUser'>Create User</NavLink>
-        <NavLink className='user-link' to='/SignIn'>Sign In</NavLink>
+        {/* <NavLink className='user-link' to='/CreateUser'>Create User</NavLink> */}
+        <button onClick={this.handleLogin} className='user-link' to='/SignIn'>Sign In</button>
       </div>
     )
   }
