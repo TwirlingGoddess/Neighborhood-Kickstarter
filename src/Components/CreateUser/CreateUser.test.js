6a653a987172b0bdeb64c2591d1c443609c6d616
Dennis <dmiller1623@gmail.com>
2018-10-31 10:00:51 -0600

    Add update selected neighborhood test for CreateUser

--- src/Components/CreateUser/CreateUser.test.js
+++ src/Components/CreateUser/CreateUser.test.js
@@ -10,7 +10,10 @@ describe('CreateUser', () => {
   })
 
   it('should update state with the neighborhood selected', () => {
+    let selectedOption = {value: 'Auraria', label: 'Aurarua', id: 1}
     wrapper = shallow(<CreateUser />)
-    wrapper.find('Select').simulate('click')
+    wrapper.instance().handleSelectChange(selectedOption)
+
+    expect(wrapper.state('neighborhood')).toEqual(selectedOption)
   })
 }) 
