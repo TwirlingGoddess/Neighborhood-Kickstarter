c1884a0e76759e8bd8cb3966651faab860013cff
Lee Graham <graham.la3@gmail.com>
2018-10-31 19:46:59 -0600

    install crypto and create String to sign

--- src/Components/CreateProject/CreateProject.js
+++ src/Components/CreateProject/CreateProject.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import './CreateProject.css';
 import deleteButton from '../../images/x-button.svg';
 import { postNewProject } from '../../utilities/apiCalls/apiCalls';
+  var crypto = require("crypto-js");
+
 
 class CreateProject extends Component {
   constructor() {
@@ -96,14 +98,17 @@ class CreateProject extends Component {
     `
     const url = 'http://neighbor-hub-images.s3.amazonaws.com'
     const settings = {
-      method: 'POST'
+      method: 'GET'
       ,
       headers: {
-        Accept: 'application/json',
+        Accept: 'application/xml',
         // 'Content-Type': 'multipart/form-data',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'If-Unmodified-Since': 'Wed, 21 Oct 2015 07:28:00 GMT',
+        'If-None-Match': '*'
+
         },
-      body: xmlImage
+      // body: image
     }
 
     try{
@@ -113,6 +118,7 @@ class CreateProject extends Component {
       console.log(data)
       const string = await (new window.DOMParser()).parseFromString(data, "text/xml")
       console.log(string)
+      console.log(string.childNodes[0].childNodes[5])
 
         if (response.ok) {
           console.log(response)
@@ -135,7 +141,21 @@ class CreateProject extends Component {
     })
   }
 
+
+function getSignatureKey(Crypto, key, dateStamp, regionName, serviceName) {
+    var kDate = Crypto.HmacSHA256(dateStamp, "AWS4" + key);
+    var kRegion = Crypto.HmacSHA256(regionName, kDate);
+    var kService = Crypto.HmacSHA256(serviceName, kRegion);
+    var kSigning = Crypto.HmacSHA256("aws4_request", kService);
+    return kSigning;
+}
+
   render() {
+    const StringToSign =
+    'AWS4-HMAC-SHA256'+
+    '20181031T193600Z'+
+    '20181031/us-west-1/neighbor-hub-images/aws4_request'+
+    '9bef5cbf5aa49f5a70e769ad4e8271843f06a84ac9248dd49fd80f18f729874f'
     return(
       <div className='create-project-section'>
         <NavLink className='view-projects-button' to='/Landing'>View Projects</NavLink>
@@ -171,10 +191,30 @@ class CreateProject extends Component {
               className="choose-file" 
               type="file"
               value=''
-              onChange={this.handlePhoto}
+              onChange={this.handlePhoto} 
+              action="http://neighbor-hub-images.s3.amazonaws.com/" 
+              method="post" 
+              encType="multipart/form-data"
             />
             <button className='add-material-button' onClick={(event) => this.addResource(event, this.state.newResource)}>Add Resource</button>
             <button className='submit-project-button' onClick={this.handleSubmit}>Submit Project</button>
+          </form>
+
+          <form action="http://neighbor-hub-images.s3.amazonaws.com/" method="post" encType="multipart/form-data">
+            <input type="input"  name="key" value="user/user1/${filename}" /><br />
+            <input type="hidden" name="acl" value="public-read" />
+            <input type="hidden" name="success_action_redirect" value="http://neighbor-hub-images.s3.amazonaws.com/successful_upload.html" />
+            <input type="input"  name="Content-Type" value="image/jpeg" /><br />
+            <input type="hidden" name="x-amz-meta-uuid" value="14365123651274" /> 
+            <input type="hidden" name="x-amz-server-side-encryption" value="AES256" /> 
+            <input type="text"   name="X-Amz-Credential" value="AKIAIXU4Q4Y4XZWUK4SQ/20151229/us-west-1/s3/aws4_request" />
+            <input type="text"   name="X-Amz-Algorithm" value="AWS4-HMAC-SHA256" />
+            <input type="text"   name="X-Amz-Date" value="20181031T193600Z" />
+            <input type="input"  name="x-amz-meta-tag" value="" /><br />
+            <input type="hidden" name="Policy" value="eyAiZXhwaXJhdGlvbiI6ICIyMDE1LTEyLTMwVDEyOjAwOjAwLjAwMFoiLA0KICAiY29uZGl0aW9ucyI6IFsNCiAgICB7ImJ1Y2tldCI6ICJzaWd2NGV4YW1wbGVidWNrZXQifSwNCiAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAidXNlci91c2VyMS8iXSwNCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LA0KICAgIHsic3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QiOiAiaHR0cDovL3NpZ3Y0ZXhhbXBsZWJ1Y2tldC5zMy5hbWF6b25hd3MuY29tL3N1Y2Nlc3NmdWxfdXBsb2FkLmh0bWwifSwNCiAgICBbInN0YXJ0cy13aXRoIiwgIiRDb250ZW50LVR5cGUiLCAiaW1hZ2UvIl0sDQogICAgeyJ4LWFtei1tZXRhLXV1aWQiOiAiMTQzNjUxMjM2NTEyNzQifSwNCiAgICB7IngtYW16LXNlcnZlci1zaWRlLWVuY3J5cHRpb24iOiAiQUVTMjU2In0sDQogICAgWyJzdGFydHMtd2l0aCIsICIkeC1hbXotbWV0YS10YWciLCAiIl0sDQoNCiAgICB7IngtYW16LWNyZWRlbnRpYWwiOiAiQUtJQUlPU0ZPRE5ON0VYQU1QTEUvMjAxNTEyMjkvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCJ9LA0KICAgIHsieC1hbXotYWxnb3JpdGhtIjogIkFXUzQtSE1BQy1TSEEyNTYifSwNCiAgICB7IngtYW16LWRhdGUiOiAiMjAxNTEyMjlUMDAwMDAwWiIgfQ0KICBdDQp9" />
+            <input type="hidden" name="X-Amz-Signature" value={StringToSign} />
+            <input type="file"   name="file" /> <br />
+            <input type="submit" name="submit" value="Upload to Amazon S3" placeholder="Upload to Amazon S3" />
           </form>
 
           <div className='listed-materials'>
