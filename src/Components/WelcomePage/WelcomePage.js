a0d00b04d9d38c570ab19afa092f582f1d582106
Dennis <dmiller1623@gmail.com>
2018-10-31 21:23:20 -0600

    Add heading to welcome page

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -29,9 +29,15 @@ class WelcomePage extends Component {
     }
     return (
       <div className='user-link-section'>
-        <NavLink className='user-link' to='/Landing'>View Projects</NavLink>
-        <NavLink className='user-link' to='/SignIn'>Sign In</NavLink>
-        <button onClick={this.handleLogin} className='user-link' to='/SignIn'>Create Account</button>
+        <div className='welcome-heading-section'>
+          <h1 className='welcome-heading'>Welome to</h1>
+          <h1 className='welcome-heading'>NeighborHub</h1>
+        </div>
+        <div className='welcome-button-section'>
+          <NavLink className='user-link' to='/Landing'>View Projects</NavLink>
+          <NavLink className='user-link' to='/SignIn'>Sign In</NavLink>
+          <button onClick={this.handleLogin} className='user-link' to='/SignIn'>Create Account</button>
+        </div>
       </div>
     );
   }
