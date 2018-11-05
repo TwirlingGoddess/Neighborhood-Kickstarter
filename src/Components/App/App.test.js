3ee365c7dd14abbcd1b0e532757cba3ca5c7f226
Lee Graham <graham.la3@gmail.com>
2018-11-04 23:12:24 -0700

    Implement test for welcome page routing in App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router';
+import WelcomePage from '../WelcomePage/WelcomePage';
+import Landing from '../Landing/Landing';
 
 describe('Contributions', () => {
   let wrapper;
+  let initial;
+  let expected;
   let mockUserState = {};
   let mockProjectState = {};
+  let mockEvent = jest.fn();
 
-  it.only('should match the snapshot', () => {
+  beforeEach(() => {
     wrapper = shallow(<App />);
+  });
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should have a valid path to the Landing', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(wrapper.find(WelcomePage)).toHaveLength(1);
+  });
+
+
 
 })
