c99179ba5b4dc409907ee12851778709c23a4eb5
Lee Graham <graham.la3@gmail.com>
2018-11-04 23:15:06 -0700

    Implement test for CreateUser routing in App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -4,6 +4,8 @@ import { shallow, mount } from 'enzyme';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router';
 import WelcomePage from '../WelcomePage/WelcomePage';
+import CreateUser from '../CreateUser/CreateUser';
+
 import Landing from '../Landing/Landing';
 
 describe('Contributions', () => {
@@ -31,6 +33,24 @@ describe('Contributions', () => {
     expect(wrapper.find(WelcomePage)).toHaveLength(1);
   });
 
+  it('should have a valid path to the Landing', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/CreateUser' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(wrapper.find(CreateUser)).toHaveLength(1);
+  });
 
+  // it.only('Should have an initial currentUser state of an empty object', () => {
+  //   expected = {};
+  //   expect(wrapper.state('currentUser')).toEqual(expected)
+  // })
+
+  // it('Should update the currentUser in state', () => {
+  //   wrapper = shallow(<App updateUser={jest.fn}/>);
+  //   wrapper.instance()
+
+  // })
 
 })
